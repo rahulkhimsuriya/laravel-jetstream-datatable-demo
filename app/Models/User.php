780a9851fe9652934315f753a9c97fba20c175c2
Rahul Khimsuriya <rahulkhimsuriya38@gmail.com>
2021-02-06 23:38:35 +0530

    change profile image url path

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,6 +59,16 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
+    /**
+     * Get the default profile photo URL if no profile photo has been uploaded.
+     *
+     * @return string
+     */
+    protected function defaultProfilePhotoUrl()
+    {
+        return 'https://ui-avatars.com/api/?name=' . urlencode($this->first_name . ' ' . $this->last_name) . '&color=7F9CF5&background=EBF4FF';
+    }
+
     public function account()
     {
         return $this->hasOne(Account::class);
